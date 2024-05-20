2a09626c49b0450008c54e25f723fdc2c1c02903
OlgaTatcher <dai.akemi230318@gmail.com>
2024-05-20 16:13:47 +0300

    third commit -added method 'numIdenticalPairs' in class 'NumberOfGoodPairs'

--- src/main/java/org/example/NumberOfGoodPairs.java
+++ src/main/java/org/example/NumberOfGoodPairs.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.HashMap;
+
+import static java.lang.System.out;
+
 public class NumberOfGoodPairs {
     public static void main(String[] args) {
+        int [] firstArrayOfNums= {1,2,3,1,1,3};
+        int resultFromFirstArrayOfNums=numIdenticalPairs(firstArrayOfNums);
+        out.println(resultFromFirstArrayOfNums);
+
+        int [] secondArrayOfNums={1,1,1,1};
+        int resultFromSecondArray=numIdenticalPairs(secondArrayOfNums);
+        out.println(resultFromSecondArray);
+
+        int [] thirdArrayOfNums={1,2,3};
+        int resultFromThirdArray=numIdenticalPairs(thirdArrayOfNums);
+        out.println(resultFromThirdArray);
 
     }
+    public static int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> numbers = new HashMap<>();
+        int result = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(numbers.containsKey(nums[i])){
+                result = result+numbers.get(nums[i]);
+                numbers.put(nums[i],numbers.get(nums[i])+1);
+            }else{
+                numbers.put(nums[i],1);
+            }
+        }
+        return result;
+    }
+
+
 }
